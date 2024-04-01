b01fe1d9dc652b3015b5cc4d3f88d5741c70fab8
ivan <kolesnik96vanya@gmail.com>
2024-04-02 01:21:14 +0300

    homework3_1

---
+++ homework3/homework3_1.js
@@ -0,0 +1,12 @@
+'use strict'
+
+function drawTriangle(height, symbol) {
+    for (let i = 1; i <= height; i++) {
+        let row = '';
+        for (let j = 1; j <= i; j++) {
+            row += symbol;
+        }
+        console.log(row);
+    }
+}
+drawTriangle(5, '*');
